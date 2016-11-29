4b552d95b5c54e0e0d99906435a81dbc44ee1789
Basith Kunimal <basith@bluroe.com>
2016-11-29 18:22:49 +0530

    ok

--- PrintPdf.java
+++ PrintPdf.java
@@ -1,45 +1,45 @@
-    import javax.print.*;
-    import javax.print.attribute.*;
-    import javax.print.attribute.standard.*;
-    import java.io.*;
-    import java.util.*;
+import javax.print.*;
+import javax.print.attribute.*;
+import javax.print.attribute.standard.*;
+import java.io.*;
+import java.util.*;
 
-    class PrintPdf {
+class PrintPdf {
 
-      static String label = "HP DeskJet 5810 series"; // change this to your printer name
+  static String label = "HP DeskJet 5810 series"; // change this to your printer name
 
-      public static void main (String [] args) throws Exception
-      {
-        // DocFlavor flavor = DocFlavor.INPUT_STREAM.PDF; // not working
-        // DocFlavor flavor = DocFlavor.INPUT_STREAM.AUTOSENSE; // not working
-        // DocFlavor flavor = DocFlavor.SERVICE_FORMATTED.PAGEABLE; // not working
-        DocFlavor flavor = new DocFlavor.INPUT_STREAM("application/octet-stream");
-        PrintRequestAttributeSet attr = new HashPrintRequestAttributeSet();
-        // attr.add(Sides.DUPLEX); // not working
-        // attr.add(MediaSize.ISO.A4); // not working
-        PrintService[] ps = PrintServiceLookup.lookupPrintServices(flavor, attr);
-        if (ps.length == 0) {
-          throw new IllegalStateException("No Printer found");
-        }
-        System.out.println("Available printers: " + Arrays.asList(ps));
+  public static void main (String [] args) throws Exception
+  {
+    // DocFlavor flavor = DocFlavor.INPUT_STREAM.PDF; // not working
+    // DocFlavor flavor = DocFlavor.INPUT_STREAM.AUTOSENSE; // not working
+    // DocFlavor flavor = DocFlavor.SERVICE_FORMATTED.PAGEABLE; // not working
+    DocFlavor flavor = new DocFlavor.INPUT_STREAM("application/octet-stream");
+    PrintRequestAttributeSet attr = new HashPrintRequestAttributeSet();
+    // attr.add(Sides.DUPLEX); // not working
+    // attr.add(MediaSize.ISO.A4); // not working
+    PrintService[] ps = PrintServiceLookup.lookupPrintServices(flavor, attr);
+    if (ps.length == 0) {
+      throw new IllegalStateException("No Printer found");
+    }
+    System.out.println("Available printers: " + Arrays.asList(ps));
 
-        PrintService myService = null;
-        for (PrintService printService : ps) {
-          if (printService.getName().equals(label)) {
-              myService = printService;
-            break;
-          }
-        }
-
-        if (myService == null) {
-            throw new IllegalStateException("Printer not found");
-        }
-
-        FileInputStream fis = new FileInputStream("./example.pdf");
-        System.out.println("doc flavour applied to printing");
-        Doc pdfDoc = new SimpleDoc(fis, flavor, null);
-        DocPrintJob printJob = myService.createPrintJob();
-        printJob.print(pdfDoc, new HashPrintRequestAttributeSet());
-        fis.close();
+    PrintService myService = null;
+    for (PrintService printService : ps) {
+      if (printService.getName().equals(label)) {
+          myService = printService;
+        break;
       }
     }
+
+    if (myService == null) {
+        throw new IllegalStateException("Printer not found");
+    }
+
+    FileInputStream fis = new FileInputStream("./example.pdf");
+    System.out.println("doc flavour applied to printing");
+    Doc pdfDoc = new SimpleDoc(fis, flavor, null);
+    DocPrintJob printJob = myService.createPrintJob();
+    printJob.print(pdfDoc, new HashPrintRequestAttributeSet());
+    fis.close();
+  }
+}
